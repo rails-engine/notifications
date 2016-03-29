f32ae59a817ec3aead841d02c7ba1adfb39bec74
Jason Lee <huacnlee@gmail.com>
2016-03-29 11:54:44 +0800

    Fix bad admin?, owner? method in controller

--- app/controllers/notifications/application_controller.rb
+++ app/controllers/notifications/application_controller.rb
@@ -1,6 +1,6 @@
 module Notifications
   class ApplicationController < ::ApplicationController
-    helper_method :current_user, :owner?, :admin?
+    helper_method :current_user
 
     alias_method :origin_current_user, Notifications.config.current_user_method.to_sym
     alias_method :origin_authenticate_user!, Notifications.config.authenticate_user_method.to_sym
